95675834dfec673b3b6760249f63f63d1f0cd317
Yure Ribeiro <yureribeirocontato@gmail.com>
2023-02-14 05:06:11 -0300

    Api CoinGecko Crypto

---
+++ src/Coin.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const styleUSD = {
+  style:"currency", currency:"USD"
+}
+
+const Coin = ({ name, image, symbol, price, volume, priceChange }) => {
+  return (
+    <div className='container-coin'>
+      <div className='coin-row'>
+        <div className='coin'>
+          <img src={image} alt="crypto" />
+          <strong>{name}</strong>
+          <p className='coin-symbol'>{symbol}</p>
+        </div>
+        <div className='coin-data'>
+          <p className='coin-price'>PREÇO: {price.toLocaleString('es-US', styleUSD)}</p> 
+          <p className='coin-volume'>VOLUME: {volume.toLocaleString('es-US', styleUSD)}</p> 
+          <p className={`coin__percent 
+            ${priceChange < 0 ? `red` : `green`}`}>
+          VARIAÇÃO 24H: {priceChange.toFixed(2)}%
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Coin
